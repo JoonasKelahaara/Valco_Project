4249ded1cdb2ff43307247890ecbceac6df8613d
Valtpit <77121949+Valtpit@users.noreply.github.com>
2023-03-01 13:07:01 +0200

    siistimistä

--- Assets/Machine2.cs
+++ Assets/Machine2.cs
@@ -7,6 +7,9 @@ public class Machine2 : MonoBehaviour
     public GameObject player;
     public GameObject machine2;
     public GameObject machine2Popup;
+    public GameObject Clock;
+    public GameObject CoinText;
+    public GameObject Player;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +22,13 @@ public class Machine2 : MonoBehaviour
     {
         if ((machine2.transform.position - player.transform.position).sqrMagnitude < 5.0f && Input.GetKeyDown(KeyCode.E))
         {
+            //ottaa muun uin pois, estää hahmoa liikkumasta ja avaa minipeli ikkunan
+            CoinText.SetActive(false);
+            Clock.SetActive(false);
+            Player.SetActive(false);
             machine2Popup.SetActive(true);
-            Debug.Log("Hello: ");
+
+
         }
             
     }
